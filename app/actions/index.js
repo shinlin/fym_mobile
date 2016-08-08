78ac46787b224e7ad980c956c41a61de3de8b5e4
Hun Kyo Jung <hunkyo.jung@gmail.com>
2016-08-08 20:39:11 +0900

    Implement Hot99 chart

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,6 +1,36 @@
 import * as types from './actionTypes';
 
-export dummyAction = (msg) => ({
-  type: types.DUMMY_ACTION,
-  msg: msg,
-});
+const requestFetch = () => {
+  return {
+    type: types.FETCH_REQUEST,
+    isFetching:true,
+  }
+}
+
+const receiveSuccess = (json) => {
+  return {
+    type: types.FETCH_SUCCESS,
+    isFetching: false,
+    items: json.top99,
+  }
+}
+
+const receiveFailure = (error) => {
+  return {
+    type: types.FETCH_FAILURE,
+    isFetching: false,
+    error,
+  }
+}
+
+var URL_HOT99 = 'http://www.feedyourmusic.com/top99';
+
+export const fetchItems = () => {
+  return (dispatch) => {
+    dispatch(requestFetch());
+    return fetch(URL_HOT99)
+      .then(request => request.json())
+      .then(json => dispatch(receiveSuccess(json)))
+      .catch(error => dispatch(receiveFailure(error)))
+  }
+}
