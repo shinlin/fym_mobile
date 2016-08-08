47a4f979b77485f27d7e47de4420e3bea4fcea76
Hun Kyo Jung <hunkyo.jung@gmail.com>
2016-08-08 15:59:44 +0900

    Tweak codes with redux

--- app/App.js
+++ app/App.js
@@ -5,21 +5,28 @@ import {
   Text,
 } from 'react-native';
 
+import { Provider } from 'react-redux';
+import configureStore from './configureStore'
+
+const store = configureStore();
+
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>      
+      </Provider>
     );
   }
 }
